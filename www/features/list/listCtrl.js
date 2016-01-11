6dc0a88611d1ba34270a2ffb8d430998f06b3979
Nick Peterson <peterson.nick2@gmail.com>
2016-01-11 13:34:29 -0700

    update listCtrl with function wrapper

--- www/features/list/listCtrl.js
+++ www/features/list/listCtrl.js
@@ -1,27 +1,32 @@
-angular.module('food-truck-finder').controller('listCtrl', function ($scope, $state, $cordovaGeolocation, mapService) {
-    
-    $scope.getData = function () {
+(function () {
+    'use strict';
 
-        mapService.getTrucks().then(function (trucks) {
-            $scope.listTrucks = trucks;
-        });
-    };
-    
-    $scope.getData();
-    
-    var getAuthedUser = function() {
-            $http.get(API_ENDPOINT.url + '/memberinfo').then(function(result) {
-            $scope.authedUser = result.data;
-            console.log("This is the authed user ", $scope.authedUser);
+    angular.module('food-truck-finder').controller('listCtrl', function ($scope, $state, $cordovaGeolocation, mapService) {
+
+        $scope.getData = function () {
+
+            mapService.getTrucks().then(function (trucks) {
+                $scope.listTrucks = trucks;
             });
         };
-    
-    console.log($scope.authedUser.user._id);
 
-    $scope.addToFavorites = function (id) {
-        mapService.addFavorite($scope.authedUser.user._id, id).then(function (response) {
-            console.log(response);
-        });
-    };
+        $scope.getData();
 
-});
+        var getAuthedUser = function () {
+            $http.get(API_ENDPOINT.url + '/memberinfo').then(function (result) {
+                $scope.authedUser = result.data;
+                console.log("This is the authed user ", $scope.authedUser);
+            });
+        };
+
+        console.log($scope.authedUser.user._id);
+
+        $scope.addToFavorites = function (id) {
+            mapService.addFavorite($scope.authedUser.user._id, id).then(function (response) {
+                console.log(response);
+            });
+        };
+
+    });
+
+})();
