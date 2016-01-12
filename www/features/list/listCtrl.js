d0fee18a118ca020cb824ed5e15c76cf42a955fe
tylermurdock <tylermurdock21@gmail.com>
2016-01-12 11:29:55 -0700

    appx address and distance is working

--- www/features/list/listCtrl.js
+++ www/features/list/listCtrl.js
@@ -1,12 +1,12 @@
-(function() {
+(function () {
     'use strict';
 
-angular.module('food-truck-finder').controller('listCtrl', function ($scope, $state, $cordovaGeolocation, mapService, userService) {
-    
-        userService.getAuthedUser().then(function(data) {
+    angular.module('food-truck-finder').controller('listCtrl', function ($scope, $state, $cordovaGeolocation, mapService, userService, $rootScope) {
+
+        userService.getAuthedUser().then(function (data) {
             $scope.authedUser = data.user;
             console.log($scope.authedUser);
-            
+
             console.log("This is the authed user ", $scope.authedUser);
             $scope.myFavorites = $scope.authedUser.favorites;
             console.log('myfaves', $scope.myFavorites);
@@ -18,54 +18,71 @@ angular.module('food-truck-finder').controller('listCtrl', function ($scope, $st
                 } else {
                     element.favStatus = false;
                 }
-            })
+            });
+
+            console.log('truckInfo', $rootScope.truckInfo);
+
+            var truckIds = $rootScope.truckInfo.map(function (element) {
+                return element.id;
+            });
+
+            $scope.listTrucks.forEach(function (element) {
+                var i = truckIds.indexOf(element._id);
+                if (i !== -1) {
+                    element.distanceFromCurrentUser = $rootScope.truckInfo[i].distanceFromCurrentUser;
+                } else{
+                    console.log('truckId not found in listTrucks');
+                }
+
+            });
+
             console.log('listTrucks ', $scope.listTrucks);
         });
-            
+
 
         mapService.getTrucks().then(function (response) {
             $scope.listTrucks = response;
             console.log("These are the list of trucks", $scope.listTrucks);
         });
 
-     $scope.toggleFavorites = function (favId, favStatus, index) {
-        $scope.listTrucks[index].favStatus = favStatus;
-        if (favStatus) {
-            console.log('remove');
-            $scope.removeFromFavorites(favId, index);
+        $scope.toggleFavorites = function (favId, favStatus, index) {
+            $scope.listTrucks[index].favStatus = favStatus;
+            if (favStatus) {
+                console.log('remove');
+                $scope.removeFromFavorites(favId, index);
 
-        } else {
-            console.log('add');
-            $scope.addToFavorites(favId, index);
+            } else {
+                console.log('add');
+                $scope.addToFavorites(favId, index);
 
-        }
-    };
-
-    $scope.addToFavorites = function (favId, index) {
-
-        var truckId = {
-            id: favId
+            }
         };
-        mapService.addFavorite($scope.authedUser._id, truckId).then(function (response) {
-            console.log(response);
-            $scope.listTrucks[index].favStatus = true;
-        }, function (err) {
-            $scope.listTrucks[index].favStatus = false;
-        });
 
-        $scope.removeFromFavorites = function (favId, index) {
+        $scope.addToFavorites = function (favId, index) {
+
             var truckId = {
                 id: favId
             };
-            mapService.removeFavorite($scope.authedUser._id, truckId).then(function (response) {
+            mapService.addFavorite($scope.authedUser._id, truckId).then(function (response) {
                 console.log(response);
-                $scope.listTrucks[index].favStatus = false;
-
-            }, function (err) {
                 $scope.listTrucks[index].favStatus = true;
+            }, function (err) {
+                $scope.listTrucks[index].favStatus = false;
             });
-        };
-    }
 
-});
+            $scope.removeFromFavorites = function (favId, index) {
+                var truckId = {
+                    id: favId
+                };
+                mapService.removeFavorite($scope.authedUser._id, truckId).then(function (response) {
+                    console.log(response);
+                    $scope.listTrucks[index].favStatus = false;
+
+                }, function (err) {
+                    $scope.listTrucks[index].favStatus = true;
+                });
+            };
+        }
+
+    });
 })();
